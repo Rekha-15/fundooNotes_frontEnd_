460bd9e4aa38ba080c18bd316596ecfd19cf4792
Rekha-15 <rekhapatil.1509@gmail.com>
2021-08-31 02:01:03 -0400

    feat: added validation for login page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-//import Login from "./pages/login";
+import Login from "./pages/login";
 import  SignUp from "./pages/sign up";
 function App() {
   return (
     <div className="App">
-     {/* <Login /> */}
+      <Login /> 
       <SignUp/>
     </div>
   );
--- my-app/src/pages/login.jsx
+++ my-app/src/pages/login.jsx
@@ -1,8 +1,11 @@
+
 import React from "react";
-import { Grid, Paper, Avatar, TextField, Button, Typography, Link,} from "@material-ui/core";
+import { Grid, Paper, Avatar, TextField, Button, Typography, Link} from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
 const Login = () => {
   const paperStyle = {
     padding: 20,
@@ -12,6 +15,19 @@ const Login = () => {
   };
   const avatarStyle = { backgroundColor: "#1bbd7e" };
   const btstyle = { margin: "8px 0" };
+  const initialValues ={
+    emailId:'',
+    password:''
+} ;
+const validationSchema=Yup.object().shape({
+  emailId:Yup.string().email('please enter valid email').required("Required"),
+  password:Yup.string().required("Required" )
+
+})
+const onSubmit=(values,props)=>{
+  console.log(values)
+  console.log(props)
+  };
   return (
     <Grid>
       <Paper elevation={10} style={paperStyle}>
@@ -21,24 +37,36 @@ const Login = () => {
           </Avatar>
           <h2>sign In</h2>
         </Grid>
-        <TextField
-          label="Username"
-          placeholder="Enter username"
+        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+          {(props)=>(
+            <Form>
+              {console.log(props)}
+        <Field as={TextField}
+          label="email Id"
+          name="emailId"
+          placeholder="Enter user emailId"
           fullWidth
           required
+          helperText={<ErrorMessage name="emailId"/>}
         />
-        <TextField
+        <Field as={TextField}
           label="Password"
+          name="password"
           placeholder="Enter password"
           type="password"
           fullWidth
           required
+          helperText={<ErrorMessage name="password"/>}
         />
-        <FormControlLabel
+        <Field as={FormControlLabel} 
           control={<Checkbox name="checkedB" color="primary" />}
           label="Remember me"
         />
         <Button type="submit" color="primary" variant="contained" style={btstyle} fullWidth> Sign in</Button>
+            </Form>
+          )
+}
+        </Formik>
         <Typography>
           <Link href="#">Forgot password</Link>
         </Typography>
