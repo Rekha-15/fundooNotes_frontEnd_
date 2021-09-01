4b8e0ee798c421c9e429c85478916c73ca4181f7
Rekha-15 <rekhapatil.1509@gmail.com>
2021-08-31 22:27:22 -0400

    Refactor: refactored validation for login page , added color for error message

--- my-app/src/pages/sign up.jsx
+++ my-app/src/pages/sign up.jsx
@@ -7,6 +7,7 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import { Formik, Field, Form} from 'formik';
 const SignUp = () => {
     const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
     const headerStyle = { margin: 0 }
@@ -23,26 +24,30 @@ const SignUp = () => {
                     <h2 style={headerStyle}>Sign Up</h2>
                     <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
                 </Grid>
-                <form>
-                    <TextField fullWidth label='firstName' placeholder="Enter your First Name" />
-                    <TextField fullWidth label='lastName' placeholder="Enter your Last Name" />
-                    <TextField fullWidth label='Email' placeholder="Please enter valid Email Id" />
-                    <FormControl component="fieldset" style={marginTop}>
-                        <FormLabel component="legend">Gender</FormLabel>
-                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                            <FormControlLabel value="other" control={<Radio />} label="Other" />
-                        </RadioGroup>
-                    </FormControl>
-                    <TextField fullWidth label='Phone Number' placeholder="Enter your valid Phone Number" />
-                    <TextField fullWidth label='Password' placeholder="Provide your password" />
-                    <FormControlLabel
-                        control={<Checkbox name="checkedA" />}
-                        label="I accept the terms and condition."
-                    />
-                    <Button type='submit' variant='contained' color='primary' style={btstyle} fullWidth>Sign Up</Button>
-                </form>
+                <Formik>
+                    {(props)=>(
+                        <form>
+                        <TextField fullWidth label='firstName' placeholder="Enter your First Name" />
+                        <TextField fullWidth label='lastName' placeholder="Enter your Last Name" />
+                        <TextField fullWidth label='Email' placeholder="Please enter valid Email Id" />
+                        <FormControl component="fieldset" style={marginTop}>
+                            <FormLabel component="legend">Gender</FormLabel>
+                            <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
+                                <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                <FormControlLabel value="other" control={<Radio />} label="Other" />
+                            </RadioGroup>
+                        </FormControl>
+                        <TextField fullWidth label='Phone Number' placeholder="Enter your valid Phone Number" />
+                        <TextField fullWidth label='Password' placeholder="Provide your password" />
+                        <FormControlLabel
+                            control={<Checkbox name="checkedA" />}
+                            label="I accept the terms and condition."
+                        />
+                        <Button type='submit' variant='contained' color='primary' style={btstyle} fullWidth>Sign Up</Button>
+                    </form>
+                    )}
+                </Formik>
             </Paper>
         </Grid>
     )
