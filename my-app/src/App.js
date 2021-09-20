7a5dbbac5ec593b7dd55e979eae481a2b04be386
Rekha-15 <rekhapatil.1509@gmail.com>
2021-09-20 04:37:21 -0400

    feat: added routing path for dashboad

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Login from "./Login/login";
 import  SignUp from "./Register/SignUp";
-
+import Dashboard from "./components/dashboard/dashboard"
 import{Route, Switch} from 'react-router-dom'
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route path='/login' component={Login}/>
         <Route path='/SignUp' component={SignUp}/>
         <Route exact path='/' component={Login}/>
-        
+        <Route path="/dashboard" component={Dashboard}/>
         )
         
         </Switch>
