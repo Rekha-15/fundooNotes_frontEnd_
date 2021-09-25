4e92fd990ea05263b628246fa7309866ac2ac290
Rekha-15 <rekhapatil.1509@gmail.com>
2021-09-25 07:51:40 -0400

    feat : added stuling page for get notes

---
+++ my-app/src/components/createNote/Notes.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import AddNotes from "./addNotes";
+import DisplayNotes from "../displayCard/displayCard";
+import Services from "../../Services/NotesServices";
+import "./Notes.scss";
+// const service = new Services();
+
+export default function Notes(props) {
+  const [notes, setNotes] = React.useState([]);
+
+  console.log("called display");
+
+  const getAllNotes = () => {
+    Services
+      .getNotes()
+      .then((res) => {
+        const {data} = res.data;
+
+        // console.log("getnotes", arrayData)
+        let notes = data.reverse();
+       console.log( "arraydata",notes);
+        setNotes(notes);
+      })
+      .catch((err) => {
+        console.log("error = " + err);
+      });
+  };
+
+  React.useEffect(() => {
+    getAllNotes();
+    console.log("called display");
+  }, []);
+
+
+  return (
+    <div className="mainContent">
+      <AddNotes getall={getAllNotes} />
+      <DisplayNotes notes={notes} getall={getAllNotes} />
+      <div>
+      </div>
+    </div>
+  );
+}
