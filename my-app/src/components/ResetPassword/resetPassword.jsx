5873500fedfa01bfa4ee2fe9ecd135cfe5697944
Rekha-15 <rekhapatil.1509@gmail.com>
2021-09-29 04:13:07 -0400

    feat : created reset password page and downloaded show unshow image for show and unshow password

---
+++ my-app/src/components/ResetPassword/resetPassword.jsx
@@ -0,0 +1,205 @@
+import "./resetPassword.scss";
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import show from "./show.png";
+import hide from "./hidden.png";
+import Services from "../../Services/NotesServices";
+
+//const services = new UserServices();
+
+export class ResetPassword extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      password: "",
+      confirmpass: "",
+      passwordError: false,
+      confirmpassError: false,
+      passtextType: "password",
+      confPassTextType: "password",
+      passIcon: hide,
+      confIcon: hide,
+    };
+  }
+
+  componentDidMount() {
+    document.title = "Reset Password";
+  }
+
+  showPass = (e) => {
+    if (e.target.name === "pass" && this.state.passIcon === hide) {
+      this.setState({
+        passtextType: "text",
+        passIcon: show,
+      });
+    } else if (e.target.name === "pass" && this.state.passIcon === show) {
+      this.setState({
+        passtextType: "password",
+        passIcon: hide,
+      });
+    } else if (e.target.name === "confirm" && this.state.confIcon === hide) {
+      this.setState({
+        confPassTextType: "text",
+        confIcon: show,
+      });
+    } else {
+      this.setState({
+        confPassTextType: "password",
+        confIcon: hide,
+      });
+    }
+  };
+
+  changeHandler = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  validation = () => {
+    let isError = false;
+    const err = this.state;
+    err.passwordError = this.state.password === "" ? true : false;
+    err.confirmpassError = this.state.confirmpass === "" ? true : false;
+    this.setState({
+      ...err,
+    });
+
+    isError = err.passwordError || err.confirmpassError;
+    return isError;
+  };
+
+  onReset = (e) => {
+    e.preventDefault();
+    console.log("val", this.validation)
+    var isInvalid = this.validation();
+    if (isInvalid) {
+      console.log("Validation failed");
+    } else if (this.state.confirmpass === this.state.password) {
+      var url = window.location.href;
+      var tokeninput = url.split("/");
+      var reqToken = tokeninput[4];
+      var token = reqToken.split("?");
+      var requiredToken = token[0];
+
+      let data = {
+        newPassword: this.state.password,
+        token: requiredToken,
+      };
+
+      Services
+        .resetPassword(data)
+        .then((res) => {
+          console.log("vbvhbh",res);
+          localStorage.setItem("token", res.data.token);
+          this.props.history.push("../login");
+        })
+        .catch((err) => {
+          console.log("The error:" + err);
+        });
+    }
+  };
+
+  render() {
+    return (
+      <div className="reset-page">
+        <form onSubmit={this.onReset} className="reset-form">
+          <div className="reset-fundoo-logo">
+            <svg className="reset-fundoo-logo" height="20" width="100">
+              <text x="8" y="19" fill="blue">
+                F
+              </text>
+              <text x="22" y="19" fill="red">
+                u
+              </text>
+              <text x="36" y="19" fill="orange">
+                n
+              </text>
+              <text x="51" y="19" fill="blue">
+                d
+              </text>
+              <text x="66" y="19" fill="green">
+                o
+              </text>
+              <text x="80" y="19" fill="red">
+                o
+              </text>
+            </svg>
+            <h3 className="reset-head">Reset Password</h3>
+            <p className="reset-tag">Reset password of your fundoo account</p>
+          </div>
+          <div className="reset-pass-div">
+            <TextField
+              name="password"
+              type={this.state.passtextType}
+              error={this.state.passwordError}
+              helperText={this.state.passwordError ? "Enter password" : ""}
+              className="reset-pass-field"
+              label="password"
+              variant="outlined"
+              onChange={this.changeHandler}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <img
+                      name="pass"
+                      className="show-icon"
+                      src={this.state.passIcon}
+                      alt=""
+                      onClick={this.showPass}
+                    />
+                  </InputAdornment>
+                ),
+              }}
+              fullWidth
+            />
+          </div>
+          <div className="reset-pass-div">
+            <TextField
+              name="confirmpass"
+              type={this.state.confPassTextType}
+              error={this.state.confirmpassError}
+              helperText={
+                this.state.confirmpassError ? "Enter Confirm password" : ""
+              }
+              className="reset-pass-field"
+              label="confirm password"
+              variant="outlined"
+              fullWidth
+              onChange={this.changeHandler}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <img
+                      name="confirm"
+                      className="show-icon"
+                      src={this.state.confIcon}
+                      alt=""
+                      onClick={(e) => this.showPass(e)}
+                    />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </div>
+
+          {this.state.password !== this.state.confirmpass ? (
+            <FormHelperText error>Password no match</FormHelperText>
+          ) : null}
+
+          <div className="reset-btn">
+            <Button type="submit" variant="contained" color="primary">
+              Reset
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ResetPassword;
